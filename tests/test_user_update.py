8ea2ae986e25b00c1ac1cdb021354b5f494e3c08
Sofya Kulachenko <kittsi2017@yandex.ru>
2025-05-26 01:51:51 +0300

    "исправлены замечани по коду"

--- tests/test_user_update.py
+++ tests/test_user_update.py
@@ -6,24 +6,36 @@ from urls import Urls
 
 
 class TestUserUpdate:
-    updated_user_data = {
-        'email': create_random_email(),
-        'password': create_random_password(),
-        'name': create_random_username()
-    }
 
     @allure.title('Проверка ответа на запрос изменения данных аутентифицированного пользователя')
     def test_update_user_authenticated_success(self, create_new_user_and_delete):
-        response = requests.patch(Urls.user_update, headers={
-            'Authorization': create_new_user_and_delete[1]['accessToken']}, data=TestUserUpdate.updated_user_data)
+        updated_user_data = {
+            'email': create_random_email(),
+            'password': create_random_password(),
+            'name': create_random_username()
+        }
+        response = requests.patch(
+            Urls.user_update,
+            headers={'Authorization': create_new_user_and_delete[1]['accessToken']},
+            data=updated_user_data
+        )
         deserials = response.json()
         assert response.status_code == 200
         assert deserials['success'] is True
-        assert deserials['user']['email'] == TestUserUpdate.updated_user_data['email']
-        assert deserials['user']['name'] == TestUserUpdate.updated_user_data['name']
+        assert deserials['user']['email'] == updated_user_data['email']
+        assert deserials['user']['name'] == updated_user_data['name']
 
     @allure.title('Проверка ответа на запрос изменения данных неаутентифицированного пользователя')
     def test_update_user_unauthenticated_expected_error(self):
-        response = requests.patch(Urls.user_update, headers=Urls.headers, data=TestUserUpdate.updated_user_data)
-        assert response.status_code == 401 and response.json() == {'success': False,
-                                                                   'message': 'You should be authorised'}
+        updated_user_data = {
+            'email': create_random_email(),
+            'password': create_random_password(),
+            'name': create_random_username()
+        }
+        response = requests.patch(
+            Urls.user_update,
+            headers=Urls.headers,
+            data=updated_user_data
+        )
+        assert response.status_code == 401
+        assert response.json() == {'success': False, 'message': 'You should be authorised'}
